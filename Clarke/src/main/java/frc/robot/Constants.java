816f1292212888198fdfad5cd3ec30c55af6c7ec
Wesley DS2 <wesley.johnson.4096@gmail.com>
2023-03-03 14:03:25 -0500

    moved balance point

--- Clarke/src/main/java/frc/robot/Constants.java
+++ Clarke/src/main/java/frc/robot/Constants.java
@@ -217,10 +217,10 @@ public final class Constants {
             Map.entry("Gamepiece4", new Pose2d(new Translation2d(6.6, 4.3), new Rotation2d(0))),
             Map.entry("StartNearBalance", new Pose2d(new Translation2d(2.52, 3.30), Rotation2d.fromDegrees(180))),
             Map.entry("StartFarBalance", new Pose2d(new Translation2d(5.6, 3.30), Rotation2d.fromDegrees(180))),
-            Map.entry("ChargerCenter", new Pose2d(new Translation2d(3.88, 3.30), Rotation2d.fromDegrees(180))),
+            Map.entry("ChargerCenter", new Pose2d(new Translation2d(3.91, 3.30), Rotation2d.fromDegrees(180))),
             Map.entry("StartNearBalanceTable", new Pose2d(new Translation2d(2.52, 2.55), Rotation2d.fromDegrees(180))),
             Map.entry("StartFarBalanceTable", new Pose2d(new Translation2d(5.6, 2.55), Rotation2d.fromDegrees(180))),
-            Map.entry("ChargerCenterTable", new Pose2d(new Translation2d(3.88, 2.55), Rotation2d.fromDegrees(180))),
+            Map.entry("ChargerCenterTable", new Pose2d(new Translation2d(3.91, 2.55), Rotation2d.fromDegrees(180))),
             Map.entry("NavPoint1", new Pose2d(new Translation2d(5.38, 4.54), Rotation2d.fromDegrees(180))),
             Map.entry("NavPoint2", new Pose2d(new Translation2d(2.8, 4.54), Rotation2d.fromDegrees(180)))
         );
@@ -274,7 +274,7 @@ public final class Constants {
         public enum PRESETS {
             HIGH_SCORE (15),
             MID_SCORE (-7),
-            LOW_SCORE (5.7 - +90),
+            LOW_SCORE (5.7 - 90),
             HANDOFF (-88),
             STOWED (ARM_LOWER_LIMIT);
             
