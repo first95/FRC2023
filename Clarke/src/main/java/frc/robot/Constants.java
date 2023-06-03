ff0cdd0535e512c769845c02a749f4edaf3e5c21
Wesley DS1 <wesley.johnson.4096@gmail.com>
2023-06-03 16:01:35 -0400

    replaced swerve module; change offset

--- Clarke/src/main/java/frc/robot/Constants.java
+++ Clarke/src/main/java/frc/robot/Constants.java
@@ -143,7 +143,7 @@ public final class Constants {
         public static final class Mod1 {
             public static final int DRIVE_MOTOR_ID = 9;
             public static final int ANGLE_MOTOR_ID = 8;
-            public static final double ANGLE_OFFSET = 180.7 + 180; // 170.69 + 180;
+            public static final double ANGLE_OFFSET = 180 + 159.12; // 170.69 + 180;
             public static final SwerveModuleConstants CONSTANTS =
                 new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, ANGLE_OFFSET);
         }
